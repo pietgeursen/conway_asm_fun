22a9ffeb78241211b8fe5b4f9dc662ed011cb75c
Piet Geursen <piet@cryptovoxels.com>
2024-10-09 23:39:34 +1300

    wip

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -7,17 +7,17 @@ fn criterion_benchmark(c: &mut Criterion) {
     //    b.iter(|| conways.next())
     //});
     c.bench_function("next_board", |b|{
-        let mut board = [0i8; Conway::BOARD_SIZE];
-        board[10] = 1;
+        let mut board = [[0; Conway::BOARD_WIDTH]; Conway::BOARD_WIDTH];
+        board[6][1] = 1;
         b.iter(||{
-            let _ = Conway::next_cell_at_index(&board, 9);
+            let _ = Conway::next_cell_at_index(&board, 6,1);
         });
     });
     c.bench_function("next_board_naiive", |b|{
-        let mut board = [0i8; Conway::BOARD_SIZE];
-        board[10] = 1;
+        let mut board = [[0; Conway::BOARD_WIDTH]; Conway::BOARD_WIDTH];
+        board[6][1] = 1;
         b.iter(||{
-            let _ = Conway::next_cell_at_index_naive(&board, 9);
+            let _ = Conway::next_cell_at_index_naive(&board, 6,1);
         });
 
     });
